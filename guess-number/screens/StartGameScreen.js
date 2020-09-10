ecf09b536b5048cee0ba8a8e1473f1e3850b1947
acslev <acslev@gmail.com>
2020-09-10 15:09:08 +0200

    Guess a Number - Confirm input validation fix

--- guess-number/screens/StartGameScreen.js
+++ guess-number/screens/StartGameScreen.js
@@ -25,7 +25,7 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 90) {
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
             Alert.alert(
                 'Invalid number!',
                 'Number has to be a number between 1 and 99.',
